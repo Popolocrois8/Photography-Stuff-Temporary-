88d86e339382723103274bff9f1d5303a1fb7bdf
Popolocrois8 <manqeem88@gmail.com>
2026-05-11 00:51:40 +0800

    Added SocialButtonGroupBrandDemo Component and made use of Untitled UI's component template

--- frontend/photoLoc/src/App.tsx
+++ frontend/photoLoc/src/App.tsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header";
+import { SocialButtonGroupBrandDemo } from "./components/SocialButtonGroupBrand";
+import { SocialButton } from "./components/base/buttons/social-button";
 
 function App() {
   return (
     <>
       <Header />
       <h1>PhotoLoc</h1>
+      <SocialButtonGroupBrandDemo />
     </>
   );
 }
